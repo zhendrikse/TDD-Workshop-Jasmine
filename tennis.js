188fc0640a2237e91e43577206b4a2c1972ab55d
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-07-15 15:54:03 +0000

    First set-up of TDD workshop

---
+++ tennis.js
@@ -0,0 +1,9 @@
+const game = {
+  score : function() {
+    return [0, 0];
+  },
+
+  status : function() {
+    return 'undecided';
+  }
+};
---
+++ tennisSpecs.js
@@ -0,0 +1,19 @@
+describe("Given an unplayed game", function () {
+  it("should score 'love' 'love'", function () {
+    expect(game.score()).toEqual([0, 0]);
+  })
+  it("should have status 'undecided'", function () {
+    expect(game.status()).toEqual('undecided')
+  })
+})
+
+describe("Given a started game", function () {
+  it("should have status 'undecided'", function () {
+    expect(game.status()).toEqual('undecided')
+  })
+  describe("And a player A has 'fourty' and plays winning ball", function () {
+    it("should have status 'won by player A'", function () {
+      expect(game.status()).toEqual('won by player A')
+    })
+  })
+})
