2505df4e93693279ed57a43605d60c65d118bb61
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-07-20 22:37:37 +0000

    Added beforeEach

--- tennisSpecs.js
+++ tennisSpecs.js
@@ -1,20 +1,33 @@
 describe("Given an unplayed game", function () {
+  var game;
+
+  beforeEach(function() {
+      game = new Game("Jan", "Piet");
+  })
+
   it("should score 'love' 'love'", function () {
-    let game = new Game("Jan", "Piet");
     expect(game.score()).toEqual([0, 0]);
   })
+  
   it("should have status 'undecided'", function () {
-    expect(new Game("Jan", "Piet").status()).toEqual('undecided')
+    expect(game.status()).toEqual('undecided')
   })
 })
 
 describe("Given a started game", function () {
-  it("should have status 'undecided'", function () {
-    expect(new Game("Jan", "Piet").status()).toEqual('undecided')
+  var game;
+
+  beforeEach(function() {
+      game = new Game("Jan", "Piet");
   })
+
+  it("should have status 'undecided'", function () {
+    expect(game.status()).toEqual('undecided')
+  })
+
   describe("And a player A has 'fourty' and plays winning ball", function () {
     it("should have status 'won by player A'", function () {
-      expect(new Game("Jan", "Piet").status()).toEqual('won by player A')
+      expect(game.status()).toEqual('won by player A')
     })
   })
 })
