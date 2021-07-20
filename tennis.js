698d377e4d29783a9f7fc050e730074372f67ded
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-07-20 22:31:13 +0000

    Defined a proper class with constructor

--- tennis.js
+++ tennis.js
@@ -1,9 +1,15 @@
-const game = {
-  score : function() {
-    return [0, 0];
-  },
+class Game {
 
-  status : function() {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  score () {
+    return [0, 0];
+  }
+
+  status () {
     return 'undecided';
   }
-};
+}
--- tennisSpecs.js
+++ tennisSpecs.js
@@ -1,19 +1,20 @@
 describe("Given an unplayed game", function () {
   it("should score 'love' 'love'", function () {
+    let game = new Game("Jan", "Piet");
     expect(game.score()).toEqual([0, 0]);
   })
   it("should have status 'undecided'", function () {
-    expect(game.status()).toEqual('undecided')
+    expect(new Game("Jan", "Piet").status()).toEqual('undecided')
   })
 })
 
 describe("Given a started game", function () {
   it("should have status 'undecided'", function () {
-    expect(game.status()).toEqual('undecided')
+    expect(new Game("Jan", "Piet").status()).toEqual('undecided')
   })
   describe("And a player A has 'fourty' and plays winning ball", function () {
     it("should have status 'won by player A'", function () {
-      expect(game.status()).toEqual('won by player A')
+      expect(new Game("Jan", "Piet").status()).toEqual('won by player A')
     })
   })
 })
